262be85fc57d5bad4e015fc9989f15fd8c7ee1d2
ProfeNancy <nancy.bernal@usm.cl>
2025-06-04 11:29:51 -0400

    UVA7 Diccionarios

---
+++ UVA7 - Diccionarios/Diccionarios.py
@@ -0,0 +1,24 @@
+# persona_lista = ["Ana",30]
+# persona_diccionario = {"nombre":"Ana", "edad":30}
+
+# persona_diccionario["apellido"] = "Bruna"
+
+# print(persona_diccionario)
+
+# del persona_diccionario["edad"]
+
+# print(persona_diccionario)
+
+#del persona_diccionario["edad"]
+# if "edad" in persona_diccionario:
+#     print(persona_diccionario["edad"])
+    
+usuarios = {
+    "user1":{"nombre":"Bruno","edad":15},
+    "user2":{"nombre":"Antonio","edad":50}
+}
+
+print(usuarios)
+print(usuarios["user2"])
+print(usuarios["user2"]["edad"])
+
